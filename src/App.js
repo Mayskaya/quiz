1a666e1ce69dc6bc146ea66a49f547c3a88f1fae
mayskaya <thekaterina295@yandex.ru>
2020-10-15 01:11:37 +0300

    add routing and 3 new future pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Layout from './hoc/Layout/Layout'
 import Quiz from './containers/Quiz/Quiz'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
+import QuizList from './containers/QuizList/QuizList'
+import Auth from './containers/Auth/Auth'
 
 function App() {
   return (
     <Layout className="Layout">
-      <Quiz />
+      <Switch>
+        <Route path={'/auth'} exact component={Auth} />
+        <Route path={'/quiz-creator'} exact component={QuizCreator} />
+        <Route path={'/quiz/:id'} exact component={Quiz} />
+        <Route path={'/'} exact component={QuizList} />
+      </Switch>
     </Layout>
   );
 }
